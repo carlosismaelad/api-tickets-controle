38c4a18262c7500458ca7e5eefc48f7072873961
carlosismaelad <carlosismaelad@outlook.com>
2024-05-01 19:14:08 -0300

    Ordering attributes of the Ticket class

--- src/entities/Ticket.js
+++ src/entities/Ticket.js
@@ -1,12 +1,12 @@
 export class Ticket{
-    constructor(id, analyst, client, description, resolutionDeadline, createdAt, updatedAt = null, images) {
+    constructor(id, analyst, client, description, createdAt, images = null, resolutionDeadline = null, updatedAt = null) {
         this.id = id
         this.analyst = analyst
         this.client = client
         this.description = description
-        this.resolutionDeadline = resolutionDeadline
         this.createdAt = createdAt
-        this.updatedAt = updatedAt
         this.images = images || []
+        this.resolutionDeadline = resolutionDeadline
+        this.updatedAt = updatedAt
     }
 }
