d5443cbcea607ff0e4390f41e4890ec8d6fa7003
carlosismaelad <carlosismaelad@outlook.com>
2024-05-01 20:20:33 -0300

    Changing the DB connection lib

---
+++ src/database/connection.js
@@ -0,0 +1,11 @@
+import pgp from 'pg-promise'
+import env from 'env-var'
+
+const db = pgp({
+  host: env.get('HOST').asString(),
+  user: env.get('USER').asString(),
+  password: env.get('PASSWORD').asString(),
+  port: env.get('PORT').asPortNumber(),
+  database: env.get('DATABASE').asString() 
+})
+
