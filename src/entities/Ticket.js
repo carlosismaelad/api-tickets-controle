7b30c03fe4765e48f3e08647bf771268539634f9
carlosismaelad <carlosismaelad@outlook.com>
2024-05-02 19:23:33 -0300

    Editing methods and adding what was missing

--- src/entities/Ticket.js
+++ src/entities/Ticket.js
@@ -1,14 +1,13 @@
 import { v4 as uuidv4 } from 'uuid'
 
 class Ticket{
-    constructor(analyst, client, description, images, resolutionDeadline, updatedAt) {
+    constructor(analyst, client, description, resolutionDeadline = null, images = null) {
         this.id = uuidv4()
         this.analyst = analyst
         this.client = client
         this.description = description
         this.resolutionDeadline = resolutionDeadline
-        this.images = images || []  
-        this.updatedAt = updatedAt
+        this.images = images !== null ? images : []  
     }
 }
 
--- src/repository/TicketsRepository.js
+++ src/repository/TicketsRepository.js
@@ -1,3 +1,4 @@
+import Ticket from '../entities/Ticket.js'
 import db from '../database/connection.js'
 
 class TicketsRepository{
@@ -6,24 +7,71 @@ class TicketsRepository{
         this.db = db
     }
 
-    async findAll(){
-        const tickets = await this.db.findAll("SELECT * FROM tickets")
-        return tickets ? tickets.map(ticketData => new Ticket(ticketData)) : null
+    async findAllTickets(){
+        try{
+            const tickets = await this.db.findAll("SELECT * FROM tickets")
+            return tickets ? tickets.map(ticketData => new Ticket(ticketData)) : null
+        }catch(error){
+            console.log('Houve um error ao listar tickets', error)
+            return {error: 'Houve um erro ao listar todos os tickets', message: error.message, success: false}
+        }
     }
 
-    async findById(id){
-        const storedTicket = await this.db.oneOrNone("SELECT * FROM tickets WHERE id = $1", id)
-        return storedTicket ? new Ticket(storedTicket) : null
+    async findTicketById(id){
+        try{
+            const storedTicket = await this.db.oneOrNone("SELECT * FROM tickets WHERE id = $1", id)
+            return storedTicket ? new Ticket(storedTicket) : null
+        }catch(error){
+            console.log('Houve um erro ao buscar o ticket.', error)
+            return {error: 'Houve um erro ao buscar o ticket', message: error.message, success: false}
+        }
     }
 
-    async save(ticket) {
-        await this.db.none("INSERT INTO tickets (id, analyst, client, description, resolutionDeadline, images) VALUES ($1, $2, $3, $4, $5, $6)", [
-            ticket.id,
-            ticket.analyst,
-            ticket.client,
-            ticket.description,
-            ticket.resolutionDeadline,
-            ticket.images
-        ]);
+    async updateTicket(ticket){
+        try{
+            const updatedTicket = await this.db.none("UPDATE tickets SET  analyst = $2, client = $3, description = $4, resolutionDeadline = $5, images = $6, updatedAt = NOW() WHERE id = $1", [
+                ticket.id,
+                ticket.analyst,
+                ticket.client,
+                ticket.description,
+                ticket.resolutionDeadline,
+                ticket.images
+            ])
+            return updatedTicket
+        }catch(error){
+            console.log('Houve um erro ao atualizar o ticket.', error)
+            return {error: 'Houve um erro ao atualizar o ticket', message: error.message, success: false}
+        }
     }
+
+    async createNewTicket(ticket) {
+        try{
+            const newTicket = await this.db.none("INSERT INTO tickets (id, analyst, client, description, resolutionDeadline, images) VALUES ($1, $2, $3, $4, $5, $6)", [
+                ticket.id,
+                ticket.analyst,
+                ticket.client,
+                ticket.description,
+                ticket.resolutionDeadline,
+                ticket.images
+            ]);
+            return newTicket
+        }catch(error){
+            console.log('Houve um erro ao salvar o ticket.', error)
+            return {error: 'Houve um erro ao salvar o ticket', message: error.message, success: false}
+        }
+    }
+
+    async deleteTicket(id){
+        try{
+            const removedTicket = await this.db.none("DELETE FROM tickets WHERE id = $1", id)
+            return removedTicket
+        }catch(error){
+            console.log('Houve um erro ao excluir o ticket.', error)
+            return {error: 'Houve um erro ao excluir o ticket', message: error.message, success: false}            
+        }
+    }
+
+
 }
+
+module.exports = TicketsRepository
