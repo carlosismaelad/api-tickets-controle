d9d7e651ee9cf14d480276ab69c61b301208e855
carlosismaelad <carlosismaelad@outlook.com>
2024-05-02 15:16:20 -0300

    Script to automate the creation of the tickets table in the connection.js file

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,4 +1,5 @@
 import pgPromise from 'pg-promise'
+import join from 'node:path'
 import env from 'env-var'
 
 const pgp = pgPromise()
@@ -10,6 +11,10 @@ const db = pgp({
   port: env.get('PORT').asPortNumber(),
   database: env.get('DATABASE').asString() 
 })
+
+const filePath = join(__dirname, "create-table.sql")
+const query = new pgp.QueryFile(filePath)
+db.query(query)
 
 module.exports = db
 
